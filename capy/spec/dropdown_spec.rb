2e6e33cb741ebd52fa701f6d6f0bb209994e6c98
Rodrigo Pereira <rodrigopbbr@gmail.com>
2022-03-17 07:55:52 -0300

    Atualização do nome do cenário

--- capy/spec/dropdown_spec.rb
+++ capy/spec/dropdown_spec.rb
@@ -1,7 +1,23 @@
-describe "Caixa de Seleção", :dropdown do
-  it "item especifico simples" do
+describe "Caixa de opções ", :dropdown do
+  it "Item especifico simples " do
     visit "https://training-wheels-protocol.herokuapp.com/dropdown"
     select("Loki", from: "dropdown")
     sleep 3
+  end
+
+  it "Item especifico com o find" do
+    visit "https://training-wheels-protocol.herokuapp.com/dropdown"
+    # Buscando elemento através da classe
+    drop = find(".avenger-list")
+    drop.find("option", text: "Scott Lang").select_option
+    sleep 3
+  end
+
+  it "Selecionar todos os itens", :sample do
+    visit "https://training-wheels-protocol.herokuapp.com/dropdown"
+    drop = find(".avenger-list")
+    # Buscar por elementos e selecionar um aleatóriamente
+    drop.all("option").sample.select_option
+    sleep 6
   end
 end
